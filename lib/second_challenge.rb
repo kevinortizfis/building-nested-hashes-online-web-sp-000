d6b044b4f37648874d7c55f3c29e5c869c494027
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-09 00:34:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       matriarch:,
       hero:,
       hero_friends:
-      }, 
+      },
     :capulet => {
       :montague => {
         patriarch:,
@@ -13,5 +13,5 @@ def second_challenge
         heroine:,
         heroine_friends:      }}
 
-  
+
 end
