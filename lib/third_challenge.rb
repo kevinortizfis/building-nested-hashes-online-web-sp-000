ce4d3c169d29a89d0756f441d44861f65f147e61
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-09 00:38:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,6 @@ def third_challenge
        :matriarch => {name: "Lady Montague", age: "54"},
        :hero => {name: "Romeo", age: "15", status: "alive"},
        :hero_friends => []
-       ]
     },
     :capulet => {
        :patriarch => {name: "Lord Capulet", age: "50"},
